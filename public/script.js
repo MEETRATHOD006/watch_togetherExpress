5ceb2d824e710fb7ac7616dec910a53d6e5dcbf2
MEETRATHOD006 <109798648+MEETRATHOD006@users.noreply.github.com>
2025-01-12 21:35:47 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -289,6 +289,9 @@ socket.on("offer", async ({ from, offer }) => {
   socket.emit("answer", { to: from, answer });
 });
 
+socket.on("User-connected", socket.id => {
+  console.log("User connected with ID:", socket.id)
+})
 // Handle incoming answer
 socket.on("answer", ({ from, answer }) => {
   const peerConnection = peers[from];
